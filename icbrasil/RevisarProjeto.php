ef75153bbaee924ea5b3b297426022a4e3d2572c
Marx Medeiros <analista.marx@gmail.com>
2016-08-07 19:29:46 -0300

    REVISÕES
    REVISADO O ITEM DO MODAL: APROVAR PROJETO
    REVISADO O LAYOUT: TOP-NAV

--- icbrasil/RevisarProjeto.php
+++ icbrasil/RevisarProjeto.php
@@ -50,121 +50,84 @@ $stmt2->execute();
       $PDF = "../Distrito/arquivo/" . $File;
 
 ?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>SIGED - Sistema Integrado de Gestão Distrital | MDIO Interact Brasil</title>
+  <meta http-equiv="Content-Language" content="pt-br">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
   <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
-  <link rel="stylesheet" href="../plugins/iCheck/flat/blue.css">
-  <link rel="stylesheet" href="../plugins/jvectormap/jquery-jvectormap-1.2.2.css">
 </head>
-<body class="hold-transition skin-blue-light fixed sidebar-mini">
+<body class="hold-transition skin-blue layout-top-nav">
 <div class="wrapper">
  <header class="main-header">
-  <a href="#" class="logo">
-   <span class="logo-mini"><img src="../dist/img/logo/ICLogoMin.png" width="45"></span>
-   <span class="logo-lg"><img src="../dist/img/logo/ICLogo.png" width="200"></span>
-  </a>
   <nav class="navbar navbar-static-top">
-   <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-    <span class="sr-only">Minimizar Navegação</span>
-   </a>
-<div class="navbar-custom-menu">
- <ul class="nav navbar-nav">
-  <li class="dropdown user user-menu">
-   <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-    <img src="../uploads/<?php echo $Foto; ?>" class="user-image" alt="User Image">
-     <span class="hidden-xs"><?php echo $LoginNome; ?></span>
-   </a>
-   <ul class="dropdown-menu">
-    <li class="user-header">
-     <img src="uploads/<?php echo $Foto; ?>" class="img-circle" alt="User Image">
-     <p>
-      <?php echo $LoginNome . " - " . $LoginCargoDistrito; ?>
-      <small>Interact Club de <?php echo $LoginClube . "<br /> Distrito " . $Distrito; ?></small>
-     </p>
-    </li>
-    <li class="user-body">
-     <div class="row">
-      <div class="col-xs-12 text-left">
-      Interact Club de <?php echo $LoginClube; ?><br />
-      <strong><?php echo $LoginCargoClube; ?></strong><br />
+   <div class="container">
+    <div class="navbar-header">
+     <span class="logo-lg"><img src="../dist/img/logo/ICLogo.png" width="200"></span>
+    </div>
+        <div class="navbar-custom-menu">
+         <ul class="nav navbar-nav">
+          <li class="dropdown user user-menu">
+           <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <img src="../uploads/<?php echo $Foto; ?>" class="user-image" alt="<?php echo $LoginNome; ?>">
+             <span class="hidden-xs"><?php echo $LoginNome; ?></span>
+           </a>
+           <ul class="dropdown-menu">
+            <li class="user-header">
+             <img src="../uploads/<?php echo $Foto; ?>" class="img-circle" alt="User Image">
+             <p>
+              <?php echo $LoginNome . " - " . $LoginCargoDistrito; ?>
+              <small>Interact Club de <?php echo $LoginClube . "<br /> Distrito " . $Distrito; ?></small>
+             </p>
+            </li>
+            <li class="user-body">
+             <div class="row">
+              <div class="col-xs-12 text-left">
+              Interact Club de <?php echo $LoginClube; ?><br />
+              <strong><?php echo $LoginCargoClube; ?></strong><br />
+              </div>
+             </div>
+            </li>
+            <li class="user-footer">
+             <div class="pull-left">
+              <a href="../MeuPerfil.php" class="btn btn-success btn-flat">Editar perfil</a>
+             </div>
+             <div class="pull-right">
+              <a href="../logout.php" class="btn btn-danger btn-flat">Sair</a>
+             </div>
+            </li>
+           </ul>
+          </li>
+         </ul>
+        </div>
       </div>
-     </div>
-    </li>
-    <li class="user-footer">
-     <div class="pull-left">
-      <a href="../MeuPerfil.php" class="btn btn-success btn-flat">Editar perfil</a>
-     </div>
-     <div class="pull-right">
-      <a href="../logout.php" class="btn btn-danger btn-flat">Sair</a>
-     </div>
-    </li>
-   </ul>
-  </li>
- </ul>
-</div>   </nav>
+    </nav>
   </header>
-  <aside class="main-sidebar">
-   <section class="sidebar">
-      <div class="user-panel">
-        <div class="pull-left image">
-          <img src="../uploads/<?php echo $Foto; ?>" class="img-circle" alt="User Image">
-        </div>
-        <div class="pull-left info">
-          <p><?php echo $LoginNome; ?></p>
-        </div>
-      </div>
-      <!-- search form -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-        Distrito <?php echo $Distrito; ?><br /><?php echo $LoginCargoDistrito; ?>
-        </div>
-      </form>     <ul class="sidebar-menu">
-      <li class="header"></li>
-      <li><a href="../dashboard.php"><i class="fa fa-home"></i>Início</a></li>
-      <li><a href="../MeuPerfil.php"><i class="fa fa-user"></i>Meu Perfil</a></li>
-      <li class="treeview">
-       <a href="#">
-        <i class="fa fa-building"></i> <span>Distrito <?php echo $Distrito; ?></span>
-         <span class="pull-right-container">
-          <i class="fa fa-angle-left pull-right"></i>
-         </span>
-       </a>
-       <ul class="treeview-menu">
-        <li><a href="Clubes.php"><i class="fa fa-industry"></i> Clubes</a></li>
-        <li><a href="Associados.php"><i class="fa fa-users"></i> Associados</a></li>
-        <li><a href="Secretaria.php"><i class="fa fa-book"></i> Secretaria</a></li>
-        <li><a href="Tesouraria.php"><i class="fa fa-dollar"></i> Tesouraria</a></li>
-       </ul>
-      </li>
-      <li class="active"><a href="Projetos.php"><i class="fa fa-archive"></i>Arquivo de Projetos</a></li>
-      <li><a href="../ImagemPublica.php"><i class="fa fa-download"></i> Material de Apoio</a></li>
-      </ul>
-    </section>
-    <!-- /.sidebar -->
-  </aside>
-
-  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    
-        <section class="content-header">
-     <div class="box box-default">
-      <div class="box-body">
-       <div class="box box-primary">
-        <div class="box-body box-profile">
-         <div class="col-xs-12">
-          <h2 class="profile-username text-center"><?php echo "[" . $Avenida . "] - " . $NomeProjeto; ?>
-            <a class="pull-right"> REVISÃO DE PROJETO </a>
-          </h2>
+    <div class="container">
+      <section class="content-header">
+        <h1>
+          Página de Revisão de Projeto
+          <small>Interact Brasil</small>
+        </h1>
+      </section>
+      <section class="content">
+       <div class="box box-warning">
+        <div class="box-header with-border">
+         <h3 class="box-title"><?php echo "[" . $Avenida . "] - " . $NomeProjeto; ?>
+         </h3>
+        <a class="pull-right">Distrito <?php echo $Distrito; ?></a>
         </div>
+        <div class="box-body">
        <div class="col-xs-5">
          <li class="list-group-item">
           <b>Código ANP:</b> 
@@ -176,8 +139,8 @@ $stmt2->execute();
           <a class="pull-right"><strong><code><?php echo $CodClube; ?> </code></strong></a>
          </li>
          <li class="list-group-item"><b>Cadastro:</b> <a class="pull-right"><?php echo $DataCadastro; ?> </i></a></li>
-
        </div>
+
        <div class="col-xs-4">
        <?php
         if ($ChamaStatus == "P") 
@@ -376,12 +339,17 @@ $stmt2->execute();
         </tbody>
         </table>
       </div><!-- /.box-body -->
-     </div> 
+
+
+
+        </div>
+        </div>
+      </section>
     </div>
-   </div>
-  </section>
   </div>
-<?php include_once '../footer.php'; ?>
+<?php
+include_once '../footer.php';
+?>
 </div>
 <script src="../plugins/jQuery/jquery-2.2.3.min.js"></script>
 <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
@@ -395,33 +363,5 @@ $stmt2->execute();
 <script src="../plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="../plugins/datatables/dataTables.bootstrap.min.js"></script>
 <script src="../plugins/fastclick/fastclick.min.js"></script>
-<script>
-      $(function () {
-        $('#clubsativos').DataTable({
-          "paging": true,
-          "lengthChange": false,
-          "searching": true,
-          "ordering": true,
-          "info": true,
-          "autoWidth": true
-        });
-        $('#clubsinativos').DataTable({
-          "paging": true,
-          "lengthChange": false,
-          "searching": true,
-          "ordering": true,
-          "info": true,
-          "autoWidth": true
-        });
-      });
-    </script>
-
-<!-- RESTO
-
-
-
-                 -->
-
-
 </body>
 </html>
