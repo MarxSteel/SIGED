b952f56b31e1177317f9de20e4e00b9d03cf6649
Marx Medeiros <analista.marx@gmail.com>
2016-08-12 10:23:02 -0300

    Correções
    Corrigido Dashboard

--- dashboard.php
+++ dashboard.php
@@ -48,23 +48,18 @@ $query->execute();
   <aside class="main-sidebar">
    <section class="sidebar">
     <?php include_once 'InfoBar.php'; ?>
-
     </section>
-    <!-- /.sidebar -->
   </aside>
-
-  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Página Inicial
-        <small>Distrito <?php echo $Distrito; ?></small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-home"></i> Início</a></li>
-        <li class="active">Distrito <?php echo $Distrito; ?></li>
-      </ol>
+   <section class="content-header">
+    <h1>
+    Página Inicial
+    <small>Distrito <?php echo $Distrito; ?></small>
+    </h1>
+    <ol class="breadcrumb">
+     <li><a href="#"><i class="fa fa-home"></i> Início</a></li>
+     <li class="active">Distrito <?php echo $Distrito; ?></li>
+    </ol>
     </section>
   <section class="content">
     <div class="row">
